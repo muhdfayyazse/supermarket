221932172731d43985545e6063baa59acce78c9c
Muhammad Fayyaz <muhammad.fayyaz.se@gmail.com>
2021-10-24 00:11:15 +0400

    initial setup

---
+++ src/NavLinks.js
@@ -0,0 +1,37 @@
+/**
+ *  header-menu and sidebar menu data
+ */
+export default [
+   {
+      "menu_title": "Home",
+      "path": "/",
+      "icon": "home",
+      "child_routes": null
+   },
+   {
+      "menu_title": "Blog",
+      "path": "/Blogsinglepage",
+      "icon": "party_mode",
+      "child_routes": null
+   },
+   {
+      "menu_title": "Contact Us",
+      "path": "/Contactus",
+      "icon": "arrow_right_alt",
+      "child_routes": null
+   },
+   {
+      "menu_title": "About Us",
+      "path": "/Aboutus",
+      "mega": true,
+      "icon": "arrow_right_alt",
+      "child_routes": null
+   },
+
+   {
+      "menu_title": "Admin Panel",
+      "path": "/admin-panel/Reports",
+      "icon": "perm_identity",
+      "child_routes": null
+   }
+]
